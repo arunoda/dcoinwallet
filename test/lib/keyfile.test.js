6107d2cbbaba503b1701c554150e146ac67ca49e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-07-24 21:49:12 +0530

    Add keyfile creation.

--- test/lib/keyfile.test.js
+++ test/lib/keyfile.test.js
@@ -1,9 +1,20 @@
-/* global describe, it */
+/* global describe, it, expect */
 
 import Keyfile from '../../lib/keyfile'
 
 describe('Keyfile', () => {
-  it('should pass', () => {
-    Keyfile.create()
+  describe('create', () => {
+    it('should contain necessory fields', () => {
+      const payload = Keyfile.create('password', { keyCount: 2 })
+
+      expect(payload.meta).toEqual({
+        network: 'testnet',
+        allowReuse: true,
+        keyCount: 2
+      })
+      expect(payload.addresses.length).toBe(2)
+      expect(typeof payload.encryptedKeyPairs).toBe('string')
+      expect(typeof payload.salt).toBe('string')
+    })
   })
 })
