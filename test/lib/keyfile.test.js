baa578c605790b94c975c37f2f0ac2873d5db199
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-07-25 06:52:10 +0530

    Implement a way to get an address from the keyfile.

--- test/lib/keyfile.test.js
+++ test/lib/keyfile.test.js
@@ -6,7 +6,6 @@ describe('Keyfile', () => {
   describe('create', () => {
     it('should contain necessory fields', () => {
       const payload = Keyfile.create('password', { keyCount: 2 })
-
       expect(payload.meta).toEqual({
         network: 'testnet',
         allowReuse: true,
@@ -15,6 +14,46 @@ describe('Keyfile', () => {
       expect(payload.addresses.length).toBe(2)
       expect(typeof payload.encryptedKeyPairs).toBe('string')
       expect(typeof payload.salt).toBe('string')
+    })
+  })
+
+  describe('getAddress', () => {
+    describe('with allowReuse', () => {
+      const payload = Keyfile.create('password', { keyCount: 2 })
+      const givenAddresses = {}
+
+      it('should give all the address in a random order', () => {
+        const keyfile = Keyfile.import(payload)
+        for (let lc = 0; lc < 100; lc++) {
+          givenAddresses[keyfile.getAddress()] = true
+        }
+
+        expect(Object.keys(givenAddresses).length).toBe(2)
+      })
+    })
+
+    describe('without allowReuse', () => {
+      const payload = Keyfile.create('password', {
+        keyCount: 2,
+        allowReuse: false
+      })
+
+      it('should not give reused addresses', () => {
+        const keyfile = Keyfile.import(payload)
+        keyfile.markAddressUsed(payload.addresses[0])
+
+        for (let lc = 0; lc < 100; lc++) {
+          expect(keyfile.getAddress()).toBe(payload.addresses[1])
+        }
+      })
+
+      it('should return null if there are no addresses', () => {
+        const keyfile = Keyfile.import(payload)
+        keyfile.markAddressUsed(payload.addresses[0])
+        keyfile.markAddressUsed(payload.addresses[1])
+
+        expect(keyfile.getAddress()).toBe(null)
+      })
     })
   })
 })
