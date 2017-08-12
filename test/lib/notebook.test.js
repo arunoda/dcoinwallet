56793aeb4c40ef120083fe32b5ffe6f403401f71
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-08-12 19:31:07 +0530

    Add support for running functions.

--- test/lib/notebook.test.js
+++ test/lib/notebook.test.js
@@ -3,45 +3,83 @@
 import Notebook from '../../lib/notebook'
 
 describe('Notebook', () => {
-  it('should support to listen to changes', (done) => {
-    const nb = new Notebook(null)
-    const item = { aa: 10 }
+  describe('items', () => {
+    it('should support to listen to changes', (done) => {
+      const nb = new Notebook(null)
+      const item = { aa: 10 }
 
-    nb.addItem({ bb: 20 })
-    nb.onChange(() => {
-      expect(nb.getItems()[1]).toEqual(item)
-      done()
+      nb.addItem({ bb: 20 })
+      nb.onChange(() => {
+        expect(nb.getItems()[1]).toEqual(item)
+        done()
+      })
+      nb.addItem(item)
     })
-    nb.addItem(item)
+
+    it('should support stop listening to changes', (done) => {
+      const nb = new Notebook(null)
+      const item = { aa: 10 }
+      let called = false
+
+      const stop = nb.onChange(() => {
+        called = true
+      })
+      stop()
+      nb.addItem(item)
+
+      setTimeout(() => {
+        expect(called).toBe(false)
+        done()
+      }, 1000)
+    })
+
+    it('should support removing all items', (done) => {
+      const nb = new Notebook(null)
+      nb.addItem({aa: 10})
+      nb.addItem({bb: 20})
+
+      expect(nb.getItems().length).toBe(2)
+      nb.onChange(() => {
+        expect(nb.getItems()).toEqual([])
+        done()
+      })
+      nb.removeAll()
+    })
   })
 
-  it('should support stop listening to changes', (done) => {
-    const nb = new Notebook(null)
-    const item = { aa: 10 }
-    let called = false
+  describe('functions', () => {
+    it('should run a registered function', async (done) => {
+      const keyfile = { name: 'Super Wallet' }
+      const nb = new Notebook(keyfile)
 
-    const stop = nb.onChange(() => {
-      called = true
+      nb.registerFunction('printName', (api) => {
+        setTimeout(() => {
+          api.addItem({
+            type: 'log',
+            message: api.keyfile.name
+          })
+        })
+        return api.keyfile.name
+      })
+
+      nb.onChange(() => {
+        expect(nb.getItems()[0]).toEqual({ type: 'log', message: keyfile.name })
+        done()
+      })
+
+      const response = await nb.run(`
+        printName()
+      `)
+      expect(response).toBe(keyfile.name)
     })
-    stop()
-    nb.addItem(item)
 
-    setTimeout(() => {
-      expect(called).toBe(false)
-      done()
-    }, 1000)
-  })
-
-  it('should support removing all items', (done) => {
-    const nb = new Notebook(null)
-    nb.addItem({aa: 10})
-    nb.addItem({bb: 20})
-
-    expect(nb.getItems().length).toBe(2)
-    nb.onChange(() => {
-      expect(nb.getItems()).toEqual([])
-      done()
+    it('should catch errors when running', (done) => {
+      const nb = new Notebook(null)
+      nb.run('someBadFunction()')
+        .catch((err) => {
+          expect(err.message).toMatch(/someBadFunction is not defined/)
+          done()
+        })
     })
-    nb.removeAll()
   })
 })
